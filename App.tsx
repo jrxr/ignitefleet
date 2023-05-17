0cad2d849a80430bc90b636cd2296dcd464f213c
agacy junior <juniormartinssom@gmail.com>
2023-05-17 14:29:09 -0300

    Creating the realm provider

--- App.tsx
+++ App.tsx
@@ -1,47 +1,49 @@
-import { ThemeProvider } from 'styled-components/native';
-import { StatusBar } from 'react-native';
-import { AppProvider, UserProvider } from '@realm/react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "react-native";
+import { AppProvider, UserProvider } from "@realm/react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import theme from './src/theme';
+import theme from "./src/theme";
 
-import { REALM_APP_ID } from '@env';
+import { REALM_APP_ID } from "@env";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
+import { RealmProvider } from "./src/libs/realm";
 
 import { SignIn } from "./src/screens/SignIn";
-import { Loading } from './src/components/Loading';
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
-  if(!fontsLoaded) {
-    return (
-      <Loading />
-    )
+  if (!fontsLoaded) {
+    return <Loading />;
   }
 
   return (
     <AppProvider id={REALM_APP_ID}>
-        <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
         <SafeAreaProvider>
-          <StatusBar 
-            barStyle="light-content" 
-            backgroundColor="transparent" 
-            translucent 
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
           />
           <UserProvider fallback={SignIn}>
-            <Routes />
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
           </UserProvider>
         </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
   );
 }
-
-
