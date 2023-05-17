98cf639dafe78e0f309d6dad4aac92574356d285
agacy junior <juniormartinssom@gmail.com>
2023-05-17 15:24:58 -0300

    Navigating to the registration screen

--- src/components/CarStatus/index.tsx
+++ src/components/CarStatus/index.tsx
@@ -1,20 +1,23 @@
-import { Key, Car } from 'phosphor-react-native';
-import { Container, IconBox, Message, TextHighlight } from './styles';
-import { useTheme } from 'styled-components';
+import { TouchableOpacityProps } from "react-native";
+import { Key, Car } from "phosphor-react-native";
+import { Container, IconBox, Message, TextHighlight } from "./styles";
+import { useTheme } from "styled-components";
 
-type Props = {
+type Props = TouchableOpacityProps & {
   licensePlate?: string | null;
-}
+};
 
-export function CarStatus({ licensePlate = null }: Props) {
-  const Icon = licensePlate ? Key : Car
-  const message = licensePlate ? `Veículo ${licensePlate} em uso. ` : 'Nenhum veículo em uso. '
-  const status = licensePlate ? 'chegada' : 'saída';
+export function CarStatus({ licensePlate = null, ...rest }: Props) {
+  const Icon = licensePlate ? Key : Car;
+  const message = licensePlate
+    ? `Veículo ${licensePlate} em uso. `
+    : "Nenhum veículo em uso. ";
+  const status = licensePlate ? "chegada" : "saída";
 
-  const theme = useTheme()
+  const theme = useTheme();
 
   return (
-    <Container>
+    <Container {...rest}>
       <IconBox>
         <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
       </IconBox>
@@ -22,9 +25,7 @@ export function CarStatus({ licensePlate = null }: Props) {
       <Message>
         {message}
 
-        <TextHighlight>
-          Clique aqui para registrar a {status}.
-        </TextHighlight>
+        <TextHighlight>Clique aqui para registrar a {status}.</TextHighlight>
       </Message>
     </Container>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 
 import { Container, Content } from './styles';
 
 export function Home() {
+  const { navigate } = useNavigation();
+
+  function handleRegisterMoviment() {
+    navigate('departure')
+  }
+
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus licensePlate="XXX-1234" />
+        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
       </Content>
     </Container>
   );
