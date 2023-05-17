efb289364c77918cde3fbed2b86deace52cf401f
agacy junior <juniormartinssom@gmail.com>
2023-05-17 14:49:05 -0300

    Creating the car status component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,16 @@
 import { HomeHeader } from '../../components/HomeHeader';
+import { CarStatus } from '../../components/CarStatus';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 export function Home() {
   return (
     <Container>
       <HomeHeader />
+
+      <Content>
+        <CarStatus licensePlate="XXX-1234" />
+      </Content>
     </Container>
   );
 }
