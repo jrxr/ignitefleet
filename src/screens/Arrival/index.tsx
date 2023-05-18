4b64e1ee9b6d7a26ddcdce0bbf7a9fdcb5e7b71b
agacy junior <juniormartinssom@gmail.com>
2023-05-18 14:19:38 -0300

    Creating Button Icon

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -11,6 +11,8 @@ import {
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { X } from "phosphor-react-native";
 
 type RouteParamProps = {
   id: string;
@@ -39,6 +41,8 @@ export function Arrival() {
         </Description>
 
         <Footer>
+          <ButtonIcon icon={X} />
+
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
