dad589c7f76e8fda3c74e441e721370950462bea
agacy junior <juniormartinssom@gmail.com>
2023-05-18 11:20:21 -0300

    Displaying bank data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,31 @@
 import { useNavigation } from '@react-navigation/native';
 
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
+
 import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 
 import { Container, Content } from './styles';
+import { useEffect } from 'react';
 
 export function Home() {
   const { navigate } = useNavigation();
+
+  const historic = useQuery(Historic)
 
   function handleRegisterMoviment() {
     navigate('departure')
   }
 
+  function fetchVehicle() {
+    console.log(historic);
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  },[])
+  
   return (
     <Container>
       <HomeHeader />
