beb691827e62d0c0ad88039e966460260d658145
agacy junior <juniormartinssom@gmail.com>
2023-05-18 14:25:24 -0300

    Loading vehicle details

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,9 @@
 import { useRoute } from "@react-navigation/native";
 
+import { useObject } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
+import { BSON } from "realm";
+
 import {
   Container,
   Content,
@@ -23,22 +27,19 @@ export function Arrival() {
 
   const { id } = route.params as RouteParamProps;
 
+  const historic = useObject(Historic, new BSON.UUID(id));
+
   return (
     <Container>
       <Header title="Chegada" />
       <Content>
         <Label>Placa do veículo</Label>
 
-        <LicensePlate>XXX0000</LicensePlate>
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
 
         <Label>Finalidade</Label>
 
-        <Description>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
-          voluptate atque necessitatibus voluptatibus eveniet rerum maiores
-          neque laborum obcaecati eos debitis deleniti tempore veritatis,
-          voluptates modi, optio ullam quasi dolor!
-        </Description>
+        <Description>{historic?.description}</Description>
 
         <Footer>
           <ButtonIcon icon={X} />
