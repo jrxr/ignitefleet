9fd71884ec579888fd09151c97bb0f82689e72f0
agacy junior <juniormartinssom@gmail.com>
2023-05-18 11:57:31 -0300

    Filtering the vehicle in use

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,37 +1,52 @@
-import { useNavigation } from '@react-navigation/native';
+import { Alert } from "react-native";
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
-import { useQuery } from '../../libs/realm';
-import { Historic } from '../../libs/realm/schemas/Historic';
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
 
-import { HomeHeader } from '../../components/HomeHeader';
-import { CarStatus } from '../../components/CarStatus';
+import { HomeHeader } from "../../components/HomeHeader";
+import { CarStatus } from "../../components/CarStatus";
 
-import { Container, Content } from './styles';
-import { useEffect } from 'react';
+import { Container, Content } from "./styles";
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
   const { navigate } = useNavigation();
 
-  const historic = useQuery(Historic)
+  const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
-    navigate('departure')
+    navigate("departure");
   }
 
   function fetchVehicle() {
-    console.log(historic);
+    try {
+      const vehicle = historic.filtered("status='departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert(
+        "Veículo em uso",
+        "Não foi possível carregar o veículo em uso."
+      );
+      console.log(error);
+    }
   }
 
   useEffect(() => {
     fetchVehicle();
-  },[])
-  
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMoviment}
+        />
       </Content>
     </Container>
   );
