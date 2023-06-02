6dd6e20adba2e1762cd055adb67846a302bed703
agacy junior <juniormartinssom@gmail.com>
2023-06-02 16:40:51 -0300

    Enabling Sync in the App

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import dayjs from "dayjs";
 
+import { useUser } from "@realm/react";
 import { useQuery, useRealm } from "../../libs/realm";
 import { Historic } from "../../libs/realm/schemas/Historic";
 
@@ -21,6 +22,7 @@ export function Home() {
   const { navigate } = useNavigation();
 
   const historic = useQuery(Historic);
+  const user = useUser();
   const realm = useRealm();
 
   function handleRegisterMoviment() {
@@ -86,6 +88,16 @@ export function Home() {
   useEffect(() => {
     fetchHistoric();
   }, [historic]);
+
+  useEffect(() => {
+    realm.subscriptions.update((mutableSubs, realm) => {
+      const historicByUserQuery = realm
+        .objects("Historic")
+        .filtered(`user_id = '${user!.id}'`);
+
+      mutableSubs.add(historicByUserQuery, { name: "hostoric_by_user" });
+    });
+  }, [realm]);
 
   return (
     <Container>
