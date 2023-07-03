16f3fea735c524f9b51cb56d1143cc88d6df2bb8
agacy junior <juniormartinssom@gmail.com>
2023-07-03 13:02:22 -0300

    Displaying marker on map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,24 +1,30 @@
-import MapView, { MapViewProps, PROVIDER_GOOGLE, LatLng } from "react-native-maps";
+import MapView, {
+  MapViewProps,
+  PROVIDER_GOOGLE,
+  LatLng,
+  Marker,
+} from "react-native-maps";
 
 type Props = MapViewProps & {
   coordinates: LatLng[];
-}
+};
 
 export function Map({ coordinates, ...rest }: Props) {
-
   const lastCoordinate = coordinates[coordinates.length - 1];
 
   return (
-    <MapView 
+    <MapView
       provider={PROVIDER_GOOGLE}
-      style={{ width: '100%', height: 200 }}
+      style={{ width: "100%", height: 200 }}
       region={{
         latitude: lastCoordinate.latitude,
         longitude: lastCoordinate.longitude,
         latitudeDelta: 0.005,
-        longitudeDelta: 0.005
+        longitudeDelta: 0.005,
       }}
       {...rest}
-    />
+    >
+      <Marker coordinate={coordinates[0]} />
+    </MapView>
   );
 }
