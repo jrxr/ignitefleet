5af3c59aac7b480f9bbe303eaae677a429a1cc14
agacy junior <juniormartinssom@gmail.com>
2023-07-17 11:13:00 -0300

    Using saveStorageLocation

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,62 +1,68 @@
-import { 
-  Accuracy, 
-  hasStartedLocationUpdatesAsync, 
-  startLocationUpdatesAsync ,
-  stopLocationUpdatesAsync
-} from 'expo-location'
-import * as TaskManager from 'expo-task-manager';
+import {
+  Accuracy,
+  hasStartedLocationUpdatesAsync,
+  startLocationUpdatesAsync,
+  stopLocationUpdatesAsync,
+} from "expo-location";
+import * as TaskManager from "expo-task-manager";
+import { saveStorageLocation } from "../libs/asyncStorage/locationStorage";
 
-export const BACKGROUND_TASK_NAME =  'location-tracking';
+export const BACKGROUND_TASK_NAME = "location-tracking";
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
-    if(error) {
-      throw error
+    if (error) {
+      throw error;
     }
 
-    const { coords, timestamp } = data.locations[0];
+    if (data) {
+      const { coords, timestamp } = data.locations[0];
 
-    const currentLocation = {
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      timestamp
+      const currentLocation = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        timestamp,
+      };
+
+      await saveStorageLocation(currentLocation);
     }
-
-    console.log(currentLocation)
 
   } catch (error) {
     console.log(error)
+    stopLocationTask();
   }
-})
+});
 
 export async function startLocationTask() {
   try {
+    const hasStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
 
-    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME)
-
-    if(hasStarted) {
+    if (hasStarted) {
       await stopLocationTask();
     }
 
     await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
       accuracy: Accuracy.Highest,
       distanceInterval: 1,
-      timeInterval: 1000
-    })
+      timeInterval: 1000,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-export async function stopLocationTask(){
+export async function stopLocationTask() {
   try {
-    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+    const hasStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
 
-    if(hasStarted) {
-      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+    if (hasStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
     }
-
   } catch (error) {
-    console.log()
+    console.log();
   }
 }
