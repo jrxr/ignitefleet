a43200a5bc1cf4cab11d528f1fd9eb3e0ec130e2
agacy junior <juniormartinssom@gmail.com>
2023-07-18 10:45:51 -0300

    Creating the Coordinate Schema

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,47 +1,60 @@
-import { Realm } from '@realm/react';
+import { Realm } from "@realm/react";
+import { CoordsSchemaProps } from "./Coords";
 
 type GenerateProps = {
   user_id: string;
   description: string;
-  license_plate: string
-}
+  license_plate: string;
+  coords: CoordsSchemaProps[];
+};
 
 export class Historic extends Realm.Object<Historic> {
-  _id!: string
-  user_id!: string
+  _id!: string;
+  user_id!: string;
   license_plate!: string;
   description!: string;
-  status!: string
+  coords!: CoordsSchemaProps[];
+  status!: string;
   created_at!: Date;
   updated_at!: Date;
 
-  static generate({ user_id,description,license_plate }: GenerateProps) {
+  static generate({
+    user_id,
+    description,
+    license_plate,
+    coords,
+  }: GenerateProps) {
     return {
       _id: new Realm.BSON.UUID(),
       user_id,
       description,
       license_plate,
-      status: 'departure',
+      coords,
+      status: "departure",
       created_at: new Date(),
       updated_at: new Date(),
-    }
+    };
   }
 
   static schema = {
-    name: 'Historic',
-    primaryKey: '_id',
+    name: "Historic",
+    primaryKey: "_id",
 
     properties: {
-      _id: 'uuid',
+      _id: "uuid",
       user_id: {
-        type: 'string',
-        indexed: true
+        type: "string",
+        indexed: true,
       },
-      license_plate: 'string',
-      description: 'string',
-      status: 'string',
-      created_at: 'date',
-      updated_at: 'date'
-    }
-  }
+      license_plate: "string",
+      description: "string",
+      coords: {
+        type: "list",
+        objectType: "Coords",
+      },
+      status: "string",
+      created_at: "date",
+      updated_at: "date",
+    },
+  };
 }
